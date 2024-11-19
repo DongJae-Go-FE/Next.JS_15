581ef2a7755d53a094fb25596305d4c9839321bc
가동재 <masterforce999@naver.com>
2024-11-19 15:11:45 +0900

    Catch All Segment Routing

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+
+//리액트 서버 컴포넌트이기 때문 async를 붙일 수 있다
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return <div>검색페이지:{q}</div>;
+}
