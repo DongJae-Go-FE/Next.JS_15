f8b14a71cd145104f2d1517ceac86628f62e615a
가동재 <masterforce999@naver.com>
2024-12-06 09:44:13 +0900

    로그인 빌드 성공

--- src/util/routes.ts
+++ src/util/routes.ts
@@ -1,6 +1,6 @@
 export const LOGIN = "/login";
 export const ROOT = "/";
 
-export const PUBLIC_ROUTES = ["/login", "/signUp", "/api/auth/register"];
+export const PUBLIC_ROUTES = ["/login", "/signUp", "/api/auth/register", "api/auth/[...nextauth]"];
 
 export const PROTECTED_SUB_ROUTES = ["/server"];
